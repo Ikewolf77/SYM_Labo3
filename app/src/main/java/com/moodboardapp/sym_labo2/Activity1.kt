7b5bbaff11a63242671028b00a1a5cfd25e65d6f
Simon <simon.mattei@heig-vd.ch>
2020-11-26 16:28:07 +0100

    missing photo and in-activity scan

--- app/src/main/java/com/moodboardapp/sym_labo2/Activity1.kt
+++ app/src/main/java/com/moodboardapp/sym_labo2/Activity1.kt
@@ -3,6 +3,7 @@ package com.moodboardapp.sym_labo2
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -12,14 +13,16 @@ import com.google.zxing.integration.android.IntentIntegrator
 class Activity1 : AppCompatActivity() {
 
     private lateinit var scanButton: Button
-    private lateinit var scanResult: TextView
+    private lateinit var scanResultTitle: TextView
+    private lateinit var scanResultImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_1)
 
         scanButton = findViewById(R.id.button_BarCode)
-        scanResult = findViewById(R.id.res_BarCode)
+        scanResultTitle = findViewById(R.id.res_title_BarCode)
+        scanResultImage = findViewById(R.id.res_image_barCode)
 
         scanButton.setOnClickListener{
             IntentIntegrator(this).initiateScan()
@@ -33,7 +36,12 @@ class Activity1 : AppCompatActivity() {
             if (result.contents == null) {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
             } else {
-                Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+
+                //set text
+                scanResultTitle.text = result.contents
+
+                //set image
+
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data)
