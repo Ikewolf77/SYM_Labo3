922dc3dd879f2c9be11718936b5b3853443a03ee
Simon <simon.mattei@heig-vd.ch>
2020-11-26 14:28:23 +0100

    temp commit

--- app/src/main/java/com/moodboardapp/sym_labo2/Activity1.kt
+++ app/src/main/java/com/moodboardapp/sym_labo2/Activity1.kt
@@ -1,13 +1,33 @@
 package com.moodboardapp.sym_labo2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
+
 
 class Activity1 : AppCompatActivity() {
     /* Code-barres */
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_1)
+
+        IntentIntegrator(this).initiateScan()
+    }
+
+    // Get the results:
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
