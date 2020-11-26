efa8b9808695d69e7ac1fda76aabd85368f6bc66
Simon <simon.mattei@heig-vd.ch>
2020-11-26 13:53:48 +0100

    push

--- app/src/main/java/com/moodboardapp/sym_labo2/Activity1.kt
+++ app/src/main/java/com/moodboardapp/sym_labo2/Activity1.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class Activity1 : AppCompatActivity() {
     /* Code-barres */
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_1)
