fab354022bcd4281172cba8145a8a2109ec9005f
Stephane Bottin <stephane.bottin@heig-vd.ch>
2020-11-29 22:35:07 +0100

    Activity init with fake account login

---
+++ app/src/main/java/com/moodboardapp/sym_labo2/NFCActivity.kt
@@ -0,0 +1,19 @@
+package com.moodboardapp.sym_labo2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+
+class NFCActivity : AppCompatActivity() {
+    /* NFC */
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_3)
+
+
+    }
+}
